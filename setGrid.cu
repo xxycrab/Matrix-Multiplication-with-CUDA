728c443f0812bb2e661ebb69c954ac689b39810c
xxycrab <xiexinyucrab@126.com>
2019-02-12 14:39:38 -0800

    Update setGrid.cu

--- setGrid.cu
+++ setGrid.cu
@@ -1,6 +1,13 @@
+#define TW BLOCKDIM_X
+
 void setGrid(int n, dim3 &blockDim, dim3 &gridDim)
 {
    // set your block dimensions and grid dimensions here
-   gridDim.x = ceil(((double)n / blockDim.x)/2);
-   gridDim.y = ceil(((double)n / blockDim.y)/2);
+   blockDim.y = (n%TW || BLOCKDIM_X != BLOCKDIM_Y*8) ? blockDim.x : blockDim.y;
+   gridDim.x = n / blockDim.x;
+   gridDim.y = n / blockDim.y;
+   if(n % blockDim.x != 0)
+   	gridDim.x++;
+   if(n % blockDim.y != 0)
+    	gridDim.y++;
 }
