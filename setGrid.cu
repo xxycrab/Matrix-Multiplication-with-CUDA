2aa2248cd09bbc5c290499d8ee0c42093fff91ff
HejaBVB09 <bhufangjw@163.com>
2019-02-18 21:15:51 -0800

    Fixed a bug.

--- setGrid.cu
+++ setGrid.cu
@@ -3,7 +3,7 @@ void setGrid(int n, dim3 &blockDim, dim3 &gridDim)
    // set your block dimensions and grid dimensions here
    gridDim.x = n / (blockDim.x * 16);
    gridDim.y = n / blockDim.y;
-   if(n % blockDim.x != 0)
+   if(n % (blockDim.x * 16) != 0)
    	gridDim.x++;
    if(n % blockDim.y != 0)
     	gridDim.y++;
