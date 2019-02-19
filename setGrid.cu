43facb8e0c90581d8207ada73ddabbef45ed7ac4
HejaBVB09 <bhufangjw@163.com>
2019-02-18 21:44:53 -0800

    set grid for 4 output.

--- setGrid.cu
+++ setGrid.cu
@@ -2,10 +2,10 @@ void setGrid(int n, dim3 &blockDim, dim3 &gridDim)
 {
     // set your block dimensions and grid dimensions here
     // remember to edit these two parameters each time you change the block size
-    gridDim.x = n / blockDim.x;
-    gridDim.y = n / (blockDim.y * 8);
-    if(n % blockDim.x != 0)
+    gridDim.x = n / (blockDim.x * 2);
+    gridDim.y = n / (blockDim.y * 2);
+    if(n % (blockDim.x*2) != 0)
         gridDim.x++;
-    if(n % (blockDim.y*8) != 0)
+    if(n % (blockDim.y*2) != 0)
         gridDim.y++;
 }
