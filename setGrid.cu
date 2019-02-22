9ec860c73e76a33e7fe8a65c552a56aca0ad62b0
xxycrab <xiexinyucrab@126.com>
2019-02-22 03:04:47 -0800

    Update setGrid.cu

--- setGrid.cu
+++ setGrid.cu
@@ -6,11 +6,11 @@ void setGrid(int n, dim3 &blockDim, dim3 &gridDim)
 {
     // set your block dimensions and grid dimensions here
     // remember to edit these two parameters each time you change the block size
-    gridDim.x = n / (blockDim.x * 4);
-    gridDim.y = n / (blockDim.y * 4);
-    if(n % (blockDim.x*4) != 0)
+    gridDim.x = n / (blockDim.x * 2);
+    gridDim.y = n / (blockDim.y * 2);
+    if(n % (blockDim.x*2) != 0)
         gridDim.x++;
-    if(n % (blockDim.y*4) != 0)
+    if(n % (blockDim.y*2) != 0)
         gridDim.y++;
     cudaSharedMemConfig  shmPreference = cudaSharedMemBankSizeEightByte;
     cudaDeviceSetSharedMemConfig(shmPreference);
